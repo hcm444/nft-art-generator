f4b5e04a4adb271da09c3f984b652cb8cedd811b
hcm444 <hcm444@gmail.com>
2022-03-22 03:34:18 -0700

    Changed generation to NFT folder since unable to upload blank directory
    Need to fix generation for NFTs. duplicate NFTs override each other
    
    can't analyze statistics until issue dealt with
    
    added comments

--- generate.py
+++ generate.py
@@ -91,9 +91,9 @@ def generate(body, eyes, mouth, nose, background):
     resized_img = attr0.resize((300, 300), Image.NEAREST)  # opensea size
     # save name as attributes number _ properties.png so I can easily perform data analysis.
     resized_img.save('NFT/{0}{1}{2}{3}{4}_{5}_{6}_{7}_{8}_{9}.png'.format(str(num_array[0]), str(num_array[1]),
-                                                                                 str(num_array[2]), str(num_array[3]),
-                                                                                 str(num_array[4]), body_value,
-                                                                                 eyes_value, mouth_value, nose_value,
-                                                                                 background_value), "PNG")
+                                                                          str(num_array[2]), str(num_array[3]),
+                                                                          str(num_array[4]), body_value,
+                                                                          eyes_value, mouth_value, nose_value,
+                                                                          background_value), "PNG")
 
     return
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from random import randint
-
+# need randint because to lazy to use seeds
 from generate import generate
 
+# generate.py
 # test classes here
 # generate 10000 of them and crash your computer!
+# created self explainatory arrays for values
 body_array = []
 
 eyes_array = []
@@ -21,7 +23,10 @@ counter = 0
 amount = 3
 
 while 0 <= counter < amount:
+    # while not at max amount of generated nfts
+    # fill respective values with random ints
     v = randint(1, 5)
+    # append ints to int arrays
     body_array.append(v)
     w = randint(1, 5)
     eyes_array.append(w)
@@ -31,16 +36,24 @@ while 0 <= counter < amount:
     nose_array.append(y)
     z = randint(1, 5)
     background_array.append(z)
-    NFT_array = [[v,w,x,y,z]]
+    # make an array of arrays representing every NFT
+    # could do analysis here
+    NFT_array = [[v, w, x, y, z]]
+    # send the data to the generator
     generate(v, w, x, y, z)
+    # apent NFT array after generation
     NFT_array.append(NFT_array)
+    # increment counter
     counter += 1
 
+# statistics arrays show a heatmap of the rarity of the NFTs
 stat_body = [body_array.count(1), body_array.count(2), body_array.count(3), body_array.count(4), body_array.count(5)]
 stat_eyes = [eyes_array.count(1), eyes_array.count(2), eyes_array.count(3), eyes_array.count(4), eyes_array.count(5)]
-stat_mouth = [mouth_array.count(1), mouth_array.count(2), mouth_array.count(3), mouth_array.count(4), mouth_array.count(5)]
+stat_mouth = [mouth_array.count(1), mouth_array.count(2), mouth_array.count(3), mouth_array.count(4),
+              mouth_array.count(5)]
 stat_nose = [nose_array.count(1), nose_array.count(2), nose_array.count(3), nose_array.count(4), nose_array.count(5)]
-stat_background = [background_array.count(1), background_array.count(2), background_array.count(3), background_array.count(4),
+stat_background = [background_array.count(1), background_array.count(2), background_array.count(3),
+                   background_array.count(4),
                    background_array.count(5)]
 
 print(amount, "NFT's generated")
@@ -49,5 +62,3 @@ print(stat_eyes)
 print(stat_mouth)
 print(stat_nose)
 print(stat_background)
-
-
